c4e7d3d353c576459aedc070eaaa2e51f2ea54ea
Leveria Volodymyr <leverya.vova123@gmail.com>
2026-02-11 13:24:07 +0200

    first commit

---
+++ src/js/2-form.js
@@ -0,0 +1,38 @@
+const formData = { email: "", message: "" };
+
+const STORAGE_KEY = "feedback-form-state";
+
+const form = document.querySelector('.feedback-form');
+
+const locStorage = localStorage.getItem(STORAGE_KEY);
+
+if (locStorage) {
+  const parsedFormData = JSON.parse(locStorage);
+formData.email = parsedFormData.email || "";
+  formData.message = parsedFormData.message || "";
+  
+  form.email.value = formData.email;
+  form.message.value = formData.message;
+}
+
+form.addEventListener("input", textInput);
+
+function textInput(event) {
+  formData[event.target.name] = event.target.value.trim();
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+
+  console.log(formData);
+}
+
+form.addEventListener("submit", submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+if (formData.email === "" || formData.message === "") {
+    alert("Fill please all fields");
+    return;
+  }
+  console.log(formData);
+event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);}
